d41cbdad6c7ab41c26bae41eccdf56a0edea0083
xhellcat1942x <75864133+xhellcat1942x@users.noreply.github.com>
2022-09-19 15:43:21 -0600

    Fixed a tag error in portfolio item

--- voodoo/src/components/PortfolioItem.js
+++ voodoo/src/components/PortfolioItem.js
@@ -4,13 +4,13 @@ function PortfolioItem(props) {
   return (
     <div className="bg-gray-700 w-fit h-fit shadow-lg rounded-xl">
       <div className="p-8">
-        <a className="flex flex-col items-center" href="">
+        <div className="flex flex-col items-center">
           <p className="pb-4">{props.title}</p>
           <a href={props.link}>
             <img className={props.imageSize} src={props.thumbnail} alt="" />
           </a>
           <p className="pt-4">{props.description}</p>
-        </a>
+        </div>
       </div>
     </div>
   );
